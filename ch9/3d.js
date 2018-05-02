35e0e07f4f940691d313058095336f88f8bec8ac
mz-hanse <hanse@mz.co.kr>
2018-05-02 16:57:13 +0900

    feat: Fix scope
    - iife로 스코프 설정
    - indicator 선택시 활성화 되도록 리팩토링

--- ch9/3d.js
+++ ch9/3d.js
@@ -15,34 +15,53 @@
   };
 }(window));
 
-var main = getEl('main');
-var sections = getEls('section');
-var indicator = getEl('.indicator');
-var sectionAngle = 90;
-var viewWidth = 0;
-var currentView = 0;
+(function() {
+  var main = getEl('main');
+  var sections = getEls('section');
+  var indicator = getEl('.indicator');
+  var sectionAngle = 90;
+  var viewWidth = 0;
+  var currentView = 0;
 
-var init = function() {
-  viewWidth = sections[0].offsetWidth;
-  sections.forEach(function(section, idx) {
-    section.setAttribute('style','transform: rotateY('+ (idx * sectionAngle) +'deg) translateZ('+ (viewWidth / 2) +'px)');
+  var init = function() {
+    viewWidth = sections[0].offsetWidth;
+    sections.forEach(function(section, idx) {
+      section.setAttribute('style','transform: rotateY('+ (idx * sectionAngle) +'deg) translateZ('+ (viewWidth / 2) +'px)');
+    });
+    rotatePage();
+  };
+
+  var rotatePage = function() {
+    main.setAttribute('style', `transform: translateZ(-${viewWidth / 2}px) rotateY(${currentView}deg)`);
+  };
+
+  var activateIndicator = function(currentLi) {
+
+    var resetHook = function(node, idx) {
+        if (node.nodeName === 'LI') {
+          node.classList.remove('is-active');
+        }
+    };
+    indicator.childNodes.forEach(resetHook);
+
+    currentLi.classList.add('is-active');
+  };
+
+  // event
+  indicator.addEventListener('click', function(e) {
+    e.preventDefault();
+    var currentLi = e.target.parentNode;
+    var liIdx = Array.prototype.indexOf.call(getEls('li', indicator), currentLi);
+
+    currentView = liIdx * sectionAngle * -1;
+
+    activateIndicator(currentLi);
+    rotatePage();
   });
-  rotatePage();
-};
 
-var rotatePage = function() {
-  main.setAttribute('style', `transform: translateZ(-${viewWidth / 2}px) rotateY(${currentView}deg)`);
-};
+  init();
 
-indicator.addEventListener('click', function(e) {
-  e.preventDefault();
-  var li = e.target.parentNode;
-  var liIdx = Array.prototype.indexOf.call(getEls('li', indicator), li);
-  currentView = liIdx * sectionAngle * -1;
+  window.addEventListener('resize', init);
+}());
 
-  rotatePage();
-});
 
-init();
-
-window.addEventListener('resize', init);
