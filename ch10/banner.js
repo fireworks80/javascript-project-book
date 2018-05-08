852ca83a5fba1b7c2e3aee3f89b07a4a130b3e0c
mz-hanse <hanse@mz.co.kr>
2018-05-08 16:23:44 +0900

    ch10 완료

--- ch10/banner.js
+++ ch10/banner.js
@@ -6,10 +6,43 @@
 	var bannerCloseBtn = wrap.querySelector('.btn-status');
 	var balloons = banner.querySelectorAll('img');
 	var wrapWidth = wrap.offsetWidth;
+	var balloonsConfig = [];
 
-	// balloons.forEach(function(balloon, idx) {
-	// 	balloon.style.left = Math.random() * ( wrapWidth - balloon.width) + 'px';
-	// });
+	var setBalloonState = function() {
+
+		balloons.forEach(function(balloon, idx) {
+			var config = {
+				x: Math.random() * (wrapWidth - balloon.width),
+				y: -120,
+				rotate: Math.random() * 360,
+				speed: Math.random() * 2 + 1
+			};
+
+			balloonsConfig.push(config);
+		});
+	};
+
+	var displayBalloons = function() {
+
+		var start = function(balloon, idx) {
+
+			if (balloon.y < 220) {
+				balloon.y += balloon.speed;
+			} else {
+				balloon.y = -120;
+			}
+
+			balloon.rotate += 5;
+
+			balloons[idx].style.top = balloon.y + 'px';
+			balloons[idx].style.left = balloon.x + 'px';
+			balloons[idx].style.transform = 'rotate('+ balloon.rotate +'deg)';
+		};
+
+		balloonsConfig.forEach(start);
+
+		requestAnimationFrame(displayBalloons);
+	};
 
 
 	soundBtn.addEventListener('click', function(e) {
@@ -27,5 +60,8 @@
 	bannerCloseBtn.addEventListener('click', function() {
 		banner.classList.toggle('is-active');
 	});
+
+	setBalloonState();
+	displayBalloons();
 
 }());
